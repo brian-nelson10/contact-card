4cb93b26880810ba66a2c610b760541f7a3080c9
brian nelson <bn3l10@gmail.com>
2022-10-14 09:13:16 -0400

    added sw

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const WorkboxPlugin = require('workbox-webpack-plugin');
+const {InjectManifest} = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
@@ -11,7 +13,34 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './index.html',
       title: 'Webpack Plugin',
-    })
+    }),
+    new InjectManifest({
+      swSrc: './src/sw.js',
+      swDest: 'service-worker.js',
+    }),
+    // new WorkboxPlugin.GenerateSW({
+    //    // Do not precache images
+    // exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+
+    // // Define runtime caching rules.
+    // runtimeCaching: [{
+    //   // Match any request that ends with .png, .jpg, .jpeg or .svg.
+    //   urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+    //   // Apply a cache-first strategy.
+    //   handler: 'CacheFirst',
+
+    //   options: {
+    //     // Use a custom cache name.
+    //     cacheName: 'images',
+
+    //     // Only cache 1 images
+    //     expiration: {
+    //       maxEntries: 1,
+    //     },
+    //   },
+    // }],
+    // })
   ],
   module: {
     rules: [
